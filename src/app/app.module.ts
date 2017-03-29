183f0d174e8cdcc6b3aa780dac58debc4b2f7654
reggmack <thegraphixninja@gmail.com>
2017-03-29 17:10:09 -0500

    Setup Router & Movie Details
    Setup Router & Movie Details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule, JsonpModule } from '@angular/http';
+import { routing } from './app.routing';
 
 import { AppComponent } from './app.component';
 import { MoviesComponent } from './components/movies/movies.component';
+import { MovieComponent } from './components/movie/movie.component';
 
 @NgModule({
   declarations: [
-    AppComponent, MoviesComponent
+    AppComponent, MoviesComponent, MovieComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    JsonpModule
+    JsonpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -25,4 +25,9 @@ export class MovieService{
         return this._jsonp.get('https://api.themoviedb.org/3/search/movie?callback=JSONP_CALLBACK&query='+searchStr+'&sort_by=popularity.desc&api_key='+this.apikey)
         .map(res => res.json());
     }
+
+    getMovie(id: string){
+        return this._jsonp.get('https://api.themoviedb.org/3/movie/'+id+'?callback=JSONP_CALLBACK&api_key='+this.apikey)
+        .map(res => res.json());
+    }
 }

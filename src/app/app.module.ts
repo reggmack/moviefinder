fdbcb231c19641ab738fdb6f0ac735d4edaefce9
reggmack <thegraphixninja@gmail.com>
2017-03-29 14:13:15 -0500

    Basic Layout Setup
    Basic Layout Setup & Movie Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MoviesComponent } from './components/movies/movies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, MoviesComponent
   ],
   imports: [
     BrowserModule,
